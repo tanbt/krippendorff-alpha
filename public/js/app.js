828407f43ac51b1c0af36053b34d322eccb126d8
Tan Bui <tan@vaadin.com>
2018-09-29 20:23:48 +0300

    Read file content to string

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,35 @@
 import Krippendorff from './krippendorff.js';
 
-const calculator = new Krippendorff();
-out(calculator._data);
+window.calculate = function calculate() {
+    var file = document.getElementById('csvInput').files[0];
+    if (!file) {
+        out('Please select a file');
+        return;
+    }
+    const fileContent = getFileContent(file);
+    if (!fileContent) {
+        out('Cannot read file content');
+        return;
+    }
 
-function out(text){
+
+    //out(fileContent);
+    // const calculator = new Krippendorff();
+    // out(calculator.data);
+}
+
+
+
+function getFileContent(fileDOM) {
+    let content = null;
+    let reader = new FileReader();
+    reader.onload = function(){
+        content = reader.result;
+    };
+    reader.readAsText(fileDOM);
+    return content;
+}
+
+function out(text) {
     document.getElementById('output').innerHTML = text;
 }
