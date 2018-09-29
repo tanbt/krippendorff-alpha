77615bad1589bacdfc133614a7f92e253e57802e
Tan Bui <tan@vaadin.com>
2018-09-29 21:29:58 +0300

    Asynchronously parse file to array and json

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 import Krippendorff from './krippendorff.js';
+//import '../lib/papaparse.js';
 
 window.calculate = function calculate() {
     var file = document.getElementById('csvInput').files[0];
@@ -6,28 +7,48 @@ window.calculate = function calculate() {
         out('Please select a file');
         return;
     }
-    const fileContent = getFileContent(file);
-    if (!fileContent) {
-        out('Cannot read file content');
-        return;
-    }
 
-
-    //out(fileContent);
-    // const calculator = new Krippendorff();
-    // out(calculator.data);
+    var promise = Promise.resolve();
+    promise.then(() => getFileContent(file))
+        .then((result) => {
+            const content = result.target.result;
+            out(JSON.stringify(parseCSVToArray(content).data));
+        });
 }
 
+function parseCSVToArray(csvString) {
+    const config = {
+        delimiter: "",	// auto-detect
+        newline: "",	// auto-detect
+        quoteChar: '"',
+        escapeChar: '"',
+        header: false,
+        trimHeaders: false,
+        dynamicTyping: false,
+        preview: 0,
+        encoding: "",
+        worker: false,
+        comments: false,
+        step: undefined,
+        complete: undefined,
+        error: undefined,
+        download: false,
+        skipEmptyLines: false,
+        chunk: undefined,
+        fastMode: undefined,
+        beforeFirstChunk: undefined,
+        withCredentials: undefined,
+        transform: undefined
+    }
+    return Papa.parse(csvString, config);
+}
 
-
-function getFileContent(fileDOM) {
-    let content = null;
-    let reader = new FileReader();
-    reader.onload = function(){
-        content = reader.result;
-    };
-    reader.readAsText(fileDOM);
-    return content;
+function getFileContent(fileDOM, output) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = resolve;
+        reader.readAsText(fileDOM);
+    });
 }
 
 function out(text) {
