bcb938b5ec09c4b865da090fa0d523ccb60c820e
Tan Bui <trungtanbui@gmail.com>
2018-10-02 23:40:09 +0300

    Add UI for selecting data type

--- src/krippendorff.js
+++ src/krippendorff.js
@@ -1,24 +1,29 @@
 import * as math from 'mathjs';
 
+const DATATYPE = {
+  categorical: 1,
+  ordinal: 2,
+  interval: 3,
+  ratio: 4,
+};
+Object.freeze(DATATYPE);
+
 /**
  * Calculate Krippendorff's Alpha of a matrix of rating table
  */
 export default class Krippendorff {
-
   /**
      * Init the calculator
      *
-     * @param {*} data An javascript 2D array
+     * @param array data An javascript 2D array
+     * @param string dataType The data type of rating
      */
-  constructor(data) {
+  constructor(data, dataType) {
+    this._dataType = DATATYPE[dataType];
+    if (!dataType) {
+      this._dataType = DATATYPE['categorical'];
+    }
     this._matrix = math.matrix(data);
   }
 
-  get matrix() {
-    return this._matrix;
-  }
-
-  set matrix(input) {
-    this._matrix = input;
-  }
 }
